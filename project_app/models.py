c4180071c0790db9e11c826fbb86a8cacf011491
bvogler10 <brinjavogler@gmail.com>
2024-11-26 11:20:35 -0500

    fixed superuser bug

--- project_app/models.py
+++ project_app/models.py
@@ -22,8 +22,8 @@ class CustomUserManager(UserManager):
         return self._create_user(name, email, password, **extra_fields)
 
     def create_superuser(self, name=None, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
         return self._create_user(name, email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
