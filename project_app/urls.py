f0a6408cbb7b7d0b5b041ac145ad4028cd69511c
bvogler10 <brinjavogler@gmail.com>
2024-12-01 23:26:46 -0500

    set up urls for registration and login

--- project_app/urls.py
+++ project_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import api
 
+from dj_rest_auth.jwt_auth import get_refresh_view
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+from rest_framework_simplejwt.views import TokenVerifyView
+
 urlpatterns = [
     path('posts', api.post_list, name='api_post_list'),
+    path('auth/register/', RegisterView.as_view(), name='rest_register'),
+    path('auth/login/', LoginView.as_view(), name='rest_login'),
+    path('auth/logout/', LogoutView.as_view(), name='rest_logout'),
 ]
